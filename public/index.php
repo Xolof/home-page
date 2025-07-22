caf9a13ef96e256bce842b914c955b08b4d140b7
Olof Johansson <olof@oljo.xyz>
2025-07-22 19:55:45 +0200

    Add Latte templating engine

--- public/index.php
+++ public/index.php
@@ -1,6 +1,14 @@
 <?php
 require "../vendor/autoload.php";
 
+use Latte\Engine;
+
+
+Flight::register("view", Engine::class, [], function ($latte) {
+    $latte->setTempDirectory(__DIR__ . '/../cache/');
+    $latte->setLoader(new \Latte\Loaders\FileLoader(__DIR__ . '/../app/views/'));
+});
+
 Flight::route("/", function () {
   echo "Flight has taken off!";
 });
