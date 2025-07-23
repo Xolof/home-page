fd9559e86a18e2d7eedfec3704f1d1a3de451a46
Olof Johansson <olof@oljo.xyz>
2025-07-23 18:58:58 +0200

    Add a route for posting the contact form

--- public/index.php
+++ public/index.php
@@ -47,4 +47,53 @@ Flight::route("/contact", function () {
     ]);
 });
 
+Flight::route('POST /contact-send', function () {
+    $request = Flight::request();
+    $name = $request->data['name'];
+    $email = $request->data['email'];
+    $message = $request->data['message'];
+    $fruit = $request->data['fruit'];
+
+    if ($fruit !== '') {
+        exit('Thank you for your message!');
+    }
+
+    $errors = [];
+
+    if ($name === '') {
+        $errors[] = "Name can not be empty.";
+    }
+
+    if ($email === '') {
+        $errors[] = "Email can not be empty.";
+    }
+
+    if ($message === '') {
+        $errors[] = "Message can not be empty.";
+    }
+
+    if (strlen($name) > 50) {
+        $errors[] = "Name can not be more than 50 characters.";
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Email must be a valid email address.";
+    }
+
+    if (strlen($message) > 500) {
+        $errors[] = "Message can not be more than 500 characters.";
+    }
+
+    if (count($errors) > 0) {
+        var_dump($errors);
+        exit("Invalid form data");
+    }
+
+    echo "OK ";
+
+    exit("Thank you for your message!");
+
+    Flight::redirect('/');
+});
+
 Flight::start();
