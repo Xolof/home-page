c9a040eb1c8c887ea54a8624cc4ec4a4b96a1a67
Olof Johansson <olof@oljo.xyz>
2025-07-23 10:50:51 +0200

    Render home view with projects and siteInfo data.

--- public/index.php
+++ public/index.php
@@ -3,14 +3,31 @@ require "../vendor/autoload.php";
 
 use Latte\Engine;
 
-
 Flight::register("view", Engine::class, [], function ($latte) {
     $latte->setTempDirectory(__DIR__ . '/../cache/');
     $latte->setLoader(new \Latte\Loaders\FileLoader(__DIR__ . '/../app/views/'));
 });
 
+Flight::map('projects', function () {
+   $file = __DIR__ . '/../data/projects.json';
+   return json_decode(file_get_contents($file), true);
+});
+
+Flight::map('siteInfo', function () {
+   $file = __DIR__ . '/../data/site.json';
+   return json_decode(file_get_contents($file), true);
+});
+
 Flight::route("/", function () {
-  echo "Flight has taken off!";
+  $projects = Flight::projects();
+  $siteInfo = Flight::siteInfo();
+
+  Flight::view()->render("home.latte", [
+    "siteOwner" =>  $siteInfo["owner"],
+    "siteTitle" =>  $siteInfo["owner"] . " | " . $siteInfo["tagline"],
+    "pageTitle" => "Home",
+    "github" => $siteInfo["github"],
+    "projects" => $projects]);
 });
 
 Flight::start();
