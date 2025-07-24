8d8b5d80e85764b53452dcf8b63fa2747d8406ce
Olof Johansson <olof@oljo.xyz>
2025-07-24 18:58:40 +0200

    Improve email headers

--- app/classes/Mailer.php
+++ app/classes/Mailer.php
@@ -6,7 +6,13 @@ class Mailer
 {
     public function send(string $receiver, string $subject, string $message, string $from)
     {
-        $headers = "From: $from";
+        $headers = [
+            'From' => $from,
+            'X-Mailer' => 'PHP/' . phpversion(),
+            'MIME-Version' => '1.0',
+            'Content-Type' => 'text/html; charset=UTF-8'
+        ];
+
         try {
             mail($receiver, $subject, $message, $headers);
             echo "Email sent to $receiver.\n";
